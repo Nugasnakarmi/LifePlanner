94609008a5919fd1b3d75c2c62d8575b9de8374d
Nugasnakarmi <damnedthinker@gmail.com>
2021-07-03 19:41:12 +1000

    updating packages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { EmailConfirmComponent } from './views/email/email-confirm/email-confirm.component';
+import { ResetPasswordComponent } from './views/email/reset-password/reset-password.component';
 import { MainViewComponent } from './views/main-view/main-view.component';
 
 const routes: Routes = [
-{path:'', component: MainViewComponent}
+{path:'', component: MainViewComponent},
+{path:'confirmEmail', component: EmailConfirmComponent},
+{path:'resetPassword', component: ResetPasswordComponent}
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,23 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainViewComponent } from './views/main-view/main-view.component';
 import {DragDropModule} from '@angular/cdk/drag-drop';
-
+import { EmailConfirmComponent } from './views/email/email-confirm/email-confirm.component';
+import { ResetPasswordComponent } from './views/email/reset-password/reset-password.component';
+import {MatFormFieldModule} from '@angular/material/form-field'; 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; 
 @NgModule({
   declarations: [
     AppComponent,
-    MainViewComponent
+    MainViewComponent,
+    EmailConfirmComponent,
+    ResetPasswordComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    DragDropModule
+    DragDropModule,
+    MatFormFieldModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
