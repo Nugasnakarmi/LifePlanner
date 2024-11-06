1847a44c35058f13f63832eb03d5974ea1afa31f
Nugasnakarmi <damnedthinker@gmail.com>
2024-11-07 01:21:33 +1000

    Add env files to repo for test

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: false,
+  SUPABASE_KEY:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjbnZkZ29xcGFkdnh5Z2xianN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA0MTU5OTQsImV4cCI6MjA0NTk5MTk5NH0.Ok5SbeonhQpPggxeHd_CM0dn0GbS2YQr8eb-rQOa9nM',
+  SUPABASE_URL: 'https://vcnvdgoqpadvxyglbjsx.supabase.co',
+  //anon-public key
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  production: false,
+  SUPABASE_KEY:
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZjbnZkZ29xcGFkdnh5Z2xianN4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzA0MTU5OTQsImV4cCI6MjA0NTk5MTk5NH0.Ok5SbeonhQpPggxeHd_CM0dn0GbS2YQr8eb-rQOa9nM',
+  SUPABASE_URL: 'https://vcnvdgoqpadvxyglbjsx.supabase.co',
+  //anon-public key
+};
