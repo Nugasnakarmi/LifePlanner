c1220fe80262340bb13249775347d3febecd63d8
Nugasnakarmi <damnedthinker@gmail.com>
2025-01-17 14:56:29 +1000

    Add password match validator to utility service

--- src/app/utility/utility.service.ts
+++ src/app/utility/utility.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { AbstractControl, ValidationErrors, ValidatorFn } from '@angular/forms';
 
 @Injectable({
   providedIn: 'root',
@@ -6,5 +7,13 @@ import { Injectable } from '@angular/core';
 export class UtilityService {
   static getEnumKeyByValue = (enumObj: any, value: number) =>
     Object.keys(enumObj).find((key) => enumObj[key] === value);
+
+  static confirmPasswordValidator: ValidatorFn = (
+    control: AbstractControl
+  ): ValidationErrors | null => {
+    return control.value.password === control.value.confirmPassword
+      ? null
+      : { PasswordNoMatch: true };
+  };
   constructor() {}
 }
